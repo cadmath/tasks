fae2fd18350e55b289d2795be1e4e17d1667f44a
cadmath <48750174+cadmath@users.noreply.github.com>
2022-01-07 15:35:16 +0300

    Update and rename task_10.html to task_10.php

--- task_10.php
+++ task_10.php
@@ -32,17 +32,27 @@
                     </div>
                     <div class="panel-container show">
                         <div class="panel-content">
-                            <div class="panel-content">
-                                <div class="form-group">
-                                    <div class="alert alert-danger fade show" role="alert">
-                                        You should check in on some of those fields below.
-                                    </div>
-                                    <form action="">
-                                        <label class="form-label" for="simpleinput">Text</label>
-                                        <input type="text" id="simpleinput" class="form-control">
-                                        <button class="btn btn-success mt-3">Submit</button>
-                                    </form>
-                                </div>
+                            <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
+                                <?php
+
+                                    $arr = [
+                                            "Privacy" => " Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
+                                                            We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
+                                                            We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
+                                                            We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.",
+                                            
+                                            "Cookies and other similar technologies" => "We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log
+                                                                                        information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services 
+                                                                                        in order to improve our website, content and services. Our use of such technologies and the data collected
+                                                                                        is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings."
+                                    ];
+
+                                    foreach($arr as $title => $article){
+                                        echo '<h2>'.$title.'</h2>'.
+                                            '<p class="mb-g">'.$article.'</p>';
+                                    }
+                            
+                                ?>
                             </div>
                         </div>
                     </div>
